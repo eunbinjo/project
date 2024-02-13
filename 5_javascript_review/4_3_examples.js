87fd6eaa6c1ee46901bd695574cf3c532b5e3c11
eunbinjo <joeunbin980916@gmail.com>
2024-02-14 08:08:31 +0900

    0213 오후

--- 5_javascript_review/4_3_examples.js
+++ 5_javascript_review/4_3_examples.js
@@ -24,3 +24,48 @@ const email2 = "test!gmail.com";
 if(email2.indexOf("@") === -1){
     console.log("올바른 이메일 형식이 아닙니다.");
 }
+
+console.log('------------------------')
+
+// Array - length
+// 배열의 길이를 이용한 반복문 처리
+const arr = [10, 20, 30];
+for(let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+}
+
+// Array - 파괴적 메서드
+// push(), pop(), unshift(), shift()
+const arr2 = [10, 20, 30, 40];
+
+arr2.push(50); // 배열 맨 뒤에 50 추가
+console.log(arr2);
+
+arr2.pop(); // 배열 맨 뒤에서 요소 추출(삭제)
+console.log(arr2);
+
+arr2.unshift(0); // 배열 맨 앞에 0 추가
+console.log(arr2);
+
+arr2.shift(); // 배열 맨 앞에서 요소 추출(삭제)
+console.log(arr2);
+
+arr2.reverse(); // 역순 정렬
+console.log(arr2);
+
+arr2.sort(); // 정렬
+console.log(arr2);
+
+// Array - 비파괴적 메서드
+// forEach()
+const arr3 = [1, 2, 3, 4, 5, 6];
+
+arr3.forEach((v) => { //콜백함수 function(v)
+    console.log(v + "안녕하세요");
+})
+
+console.log('------------------------')
+
+const date = new Date();
+console.log(date);
+
